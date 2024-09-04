10f057fbabf60779ebda6012a4ad42b81589d973
QuinnQwerty <quinnklez@gmail.com>
2024-09-04 03:50:48 -0500

    Progress on main page

--- sketch.js
+++ sketch.js
@@ -1,13 +1,35 @@
+let things = [];
+
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    background(200,120,120);
 }
 
 function draw(){
-    ellipse(mouseX,mouseY,mouseX,mouseY);
+    background(66,166,166);
+
+    for(let i = 0; i < things.length; i++){
+        noStroke();
+        fill(26,106,106);
+        ellipse(things[i].x,things[i].y,things[i].size,things[i].size);
+    }
+
+    noStroke();
+    fill(26,106,106);
+    rect(0,0,width,250);
+}
+
+function mousePressed(){
+    things[things.length] = new thing(mouseX, mouseY);
 }
 
 function windowResized(){
     resizeCanvas(windowWidth,windowHeight);
-    background(200,120,120);
+}
+
+class thing{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.size = 50;
+    }
 }
